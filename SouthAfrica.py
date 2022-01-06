b102960a6559ef4761b6c6432ee1fae27b01c7e0
MangoErik <68139423+MangoErik@users.noreply.github.com>
2022-01-05 19:55:40 -0800

    Update SouthAfrica.py

--- SouthAfrica.py
+++ SouthAfrica.py
@@ -8,6 +8,10 @@ import json
 import os
 import pandas as pd
 
+#added libraries
+import datetime
+import glob
+
 def createFolders():
 	if not os.path.isdir('municiple_data'):
 		os.mkdir('municiple_data')
@@ -18,11 +22,11 @@ def municipleData():
 	#count iterates through each subset of modified data
 	count = 0
 
-	#url_index iteraates through the list of json webpages
+	#url_index iterates through the list of json webpages
 	url_index = 1
 
-	url1 = "https://nmclist.nicd.ac.za/App_JSON/DashProvincialCumulativeTests_"
-	url2 = ".json?v=2025"
+	url1 = 'https://nmclist.nicd.ac.za/App_JSON/DashProvincialCumulativeTests_'
+	url2 = '.json?v=2025'
 
 	for url_index in range(1,10):
 		#iterates through json webpages
@@ -32,20 +36,35 @@ def municipleData():
 		#processes json response
 		response = requests.get(url)
 		data_json = json.loads(response.text)
-		date_of_collection = data_json["start"]
-		modified_data = data_json["series"]
+		date_of_collection = data_json['start']
+		modified_data = data_json['series']
 		url_index+=1
 			
 		max_pages = len(modified_data)
 		for count in range(0,max_pages):
 			#parsed info starting from count
-			sub_modified_data = modified_data[count]["data"]
+			sub_modified_data = modified_data[count]['data']
 
 			#parses name info from count
-			municiple_name = modified_data[count]["name"]
+			municiple_name = modified_data[count]['name']
 
+			#finds the code for the province by using the province as a key in a dictionary
+			with open('province_municiple_list.txt') as f:
+				name_list = f.read()
+				modified_list = json.loads(name_list)
+			municiple_code = modified_list.get(municiple_name)
 			municiple_csv = pd.DataFrame(sub_modified_data, columns = ['ID', 'Cases'])
-			municiple_csv.to_csv(f'data/municiple_data/{municiple_name}.csv')
+
+			#inverts data so that it begins from most recent cumulative data instead of starting from case 1
+			municiple_csv = municiple_csv.sort_values(by=['Cases'], ascending=False)
+
+			#adds row to top with municiple code and name
+			new_row = pd.DataFrame({'ID': [municiple_code], 'Cases': [municiple_name]})
+			municiple_csv = pd.concat([new_row, municiple_csv]).reset_index(drop=True)
+
+			municiple_csv.to_csv(f'data/municiple_data/{municiple_code}.csv', index=False)
+
+			#print(municiple_name)
 
 def provinceData():
 	count = 0
@@ -54,24 +73,55 @@ def provinceData():
 	#retrieves info from json request
 	response = requests.get(url)
 	data_json = json.loads(response.text)
-	date_of_collection = data_json["start"]
-	modified_data = data_json["series"]
+	date_of_collection = data_json['start']
+	modified_data = data_json['series']
+
+	#converts timestamp to iso format
+	date_obj = datetime.datetime.strptime(date_of_collection, "%Y-%m-%d %H:%M:%S")
+	iso_date = date_obj.isoformat()
 	
 	max_pages = len(modified_data)
 	for count in range(0,max_pages):
 		#parsed info starting from count
-		sub_modified_data = modified_data[count]["data"]
+		sub_modified_data = modified_data[count]['data']
 
 		#parses name info from count
-		province_name = modified_data[count]["name"]
+		province_name = modified_data[count]['name']
 
+		#finds the code for the province by using the province as a key in a dictionary
+		with open('province_municiple_list.txt') as f:
+			name_list = f.read()
+			modified_list = json.loads(name_list)
+		province_code = modified_list.get(province_name)
 		province_csv = pd.DataFrame(sub_modified_data, columns = ['ID', 'Cases'])
-		province_csv.to_csv(f'data/province_data/{province_name}.csv')
+
+		#inverts data so that it begins from most recent cumulative data instead of starting from case 1
+		province_csv = province_csv.sort_values(by=['Cases'], ascending=False)
+
+		#adds row to top with province code and name
+		new_row = pd.DataFrame({'ID': [province_code], 'Cases': [province_name]})
+		province_csv = pd.concat([new_row, province_csv]).reset_index(drop=True)
+
+		province_csv.to_csv(f'data/province_data/{province_code}.csv', index=False)
+
+		#print(province_name)
+
+def joinFiles():
+	joined_municiple_files = os.path.join('data/municiple_data', '*.csv')
+	joined_municiple_list = glob.glob(joined_municiple_files)
+	total_municiple_info = pd.concat(map(pd.read_csv, joined_municiple_list), ignore_index=True, axis=1) 
+	total_municiple_info.to_csv('data/total_info/total_municiple_info.csv', index=False)
+
+	joined_province_files = os.path.join('data/province_data', '*.csv')
+	joined_province_list = glob.glob(joined_province_files)
+	total_province_info = pd.concat(map(pd.read_csv, joined_province_list), ignore_index=True, axis=1)
+	total_province_info.to_csv('data/total_info/total_province_info.csv', index=False)
 
 def main():
 	#createFolders() only nessecary if running code on own computer
 	municipleData()
 	provinceData()
+	joinFiles()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
